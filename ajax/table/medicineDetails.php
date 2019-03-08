b33e5106ac1c362541c95eddca45c43ae34c1d89
williamboco <yamyamboco21@gmail.com>
2019-03-08 21:32:51 +0800

    Medicine Details Has been updated

--- ajax/table/medicineDetails.php
+++ ajax/table/medicineDetails.php
@@ -1,10 +1,10 @@
 <?php
 	include('../../includes/dbcon.php');
 	$medId = $_GET['id'];
-	
+
 	$query = "SELECT * FROM `medicine` WHERE ID='$medId'";
 	$result = mysqli_query($con, $query);
-	
+
 	$res = mysqli_fetch_array($result);
 	$content = '';
 	if($res['brandName'] != '') {
@@ -13,14 +13,16 @@
 		$content .= '<p class="text-center"><span class="lead">'.$res['genericName'].'</span></p>';
 	if($res['isSupply'] != '0') {
 		$content .= '<p class="text-center">Type: <span class="lead">Supply</span></p>';
+			$content .= '<p class="text-center">Quantity: <span class="lead">'.$res['thresholdQty'].'</span></p>';
 	}else {
 		$content .= '<p class="text-center">Type: <span class="lead">Drug</span></p>';
 		$content .= '<p class="text-center">Form: <span class="lead">'.$res['type'].'</span></p>';
+		$content .= '<p class="text-center">Quantity: <span class="lead">'.$res['thresholdQty'].'</span></p>';
 	}
-	
+
 	if($content == '') {
 		$content .= "<p class='text-center lead'>No Details</p>";
 	}
-	
+
 	echo $content;
 ?>
