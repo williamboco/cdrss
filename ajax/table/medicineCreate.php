b600cc610356055513f68b0dd6dd6d3836bb7ad9
williamboco <yamyamboco21@gmail.com>
2019-03-08 21:55:08 +0800

    medicineCreate.php has been updated

--- ajax/table/medicineCreate.php
+++ ajax/table/medicineCreate.php
@@ -1,25 +1,26 @@
 <?php
 	include('../../includes/dbcon.php');
 	$medicineType = $_GET['medicineType'];
-	
+	$thresholdQty = $_GET['thresholdQty'];
+
 	if($medicineType == '0') {
 		$brandName = $_GET['brandName'];
 		$genericName = $_GET['genericName'];
 		$medicineForm = $_GET['medicineForm'];
-		
-		$query = "INSERT INTO `medicine` (`ID`, `genericName`, `brandName`, `type`, isSupply, `isDeleted`) VALUES (NULL, '$genericName', '$brandName', '$medicineForm', '$medicineType', '0')";
-		
+
+		$query = "INSERT INTO `medicine` (`ID`, `genericName`, `brandName`, `type`, isSupply, `isDeleted`, `thresholdQty`) VALUES (NULL, '$genericName', '$brandName', '$medicineForm', '$medicineType', '0', '$thresholdQty')";
+
 		$query0 = "SELECT * FROM medicine WHERE brandName='$brandName' AND genericName='$genericName'";
-		
+
 	}else {
 		$supplyName = $_GET['supplyName'];
-		
-		$query = "INSERT INTO `medicine` (`ID`, `genericName`, `brandName`, `type`, isSupply, `isDeleted`) VALUES (NULL, '$supplyName', '', '', '$medicineType', '0')";
-		
+
+		$query = "INSERT INTO `medicine` (`ID`, `genericName`, `brandName`, `type`, isSupply, `isDeleted`, `thresholdQty`) VALUES (NULL, '$supplyName', '', '', '$medicineType', '0', '$thresholdQty')";
+
 		$query0 = "SELECT * FROM medicine WHERE genericName='$supplyName'";
 	}
-	
-	
+
+
 	if ($result=mysqli_query($con, $query0)) {
 		if(mysqli_num_rows($result) > 0) {
 			$row = mysqli_fetch_array($result);
@@ -45,6 +46,6 @@
 	}else {
 		echo "Query Failed";
 	}
-	
-	
+
+
 ?>
