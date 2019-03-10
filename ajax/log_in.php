9eb5737b3801df03d00e350648b16df845dd6d80
williamboco <yamyamboco21@gmail.com>
2019-03-10 18:57:05 +0800

    log_in.php has been updated

--- ajax/log_in.php
+++ ajax/log_in.php
@@ -12,11 +12,11 @@ $row= mysqli_fetch_array($query);
 
 $message = array();
 if(mysqli_num_rows($query) > 0 /*&& verify($password, $row['password'])*/) {
-	$_SESSION['userID']=$row['ID'];
+	$_SESSION['userID']=$row['userID'];
 	$_SESSION['firstName']=$row['firstName'];
 	$_SESSION['role']=$row['role'];
 
-	array_push($message, 'success');
+	array_push($message, 'Success');
 	array_push($message, $row['role']);
 
 }else {
