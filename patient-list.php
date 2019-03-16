76d9192a9aedd57286ae06e014894281578896d3
williamboco <yamyamboco21@gmail.com>
2019-03-16 09:23:50 +0800

    patient-list.php

--- patient-list.php
+++ patient-list.php
@@ -16,7 +16,7 @@ if($_SESSION['role'] == 'Admin') {
     <link href="vendor/DataTables-1.10.15/media/css/jquery.dataTables.min.css" rel="stylesheet">
 	  <link href="vendor/select2-4.0.3/dist/css/select2.min.css" rel="stylesheet">
 	  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
+    <link href="css/mains.css" rel="stylesheet">
 		<link href="css/animate.css" rel="stylesheet">
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
