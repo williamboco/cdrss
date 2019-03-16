57d38a37fc0231f92d4b05958fa4ebb58144986e
williamboco <yamyamboco21@gmail.com>
2019-03-16 22:35:16 +0800

    prepared statements

--- ajax/table/medicineCreate.php
+++ ajax/table/medicineCreate.php
@@ -1,51 +1,60 @@
 <?php
 	include('../../includes/dbcon.php');
 	$medicineType = $_GET['medicineType'];
-	$thresholdQty = $_GET['thresholdQty'];
+
 
 	if($medicineType == '0') {
+
+		$stmt = $con->prepare("INSERT INTO medicine (ID, genericName, brandName, type, isSupply, isDeleted, thresholdQty) VALUES (?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("isssiii", $isNull, $brandName, $genericName, $medicineForm, $medicineType, $isSupply, $thresholdQty);
+
 		$brandName = $_GET['brandName'];
 		$genericName = $_GET['genericName'];
 		$medicineForm = $_GET['medicineForm'];
-
-		$query = "INSERT INTO `medicine` (`ID`, `genericName`, `brandName`, `type`, isSupply, `isDeleted`, `thresholdQty`) VALUES (NULL, '$genericName', '$brandName', '$medicineForm', '$medicineType', '0', '$thresholdQty')";
+		$thresholdQty = $_GET['thresholdQty'];
+		$isSupply = 0;
+		$isNull = NULL;
 
 		$query0 = "SELECT * FROM medicine WHERE brandName='$brandName' AND genericName='$genericName'";
 
 	}else {
-		$supplyName = $_GET['supplyName'];
 
-		$query = "INSERT INTO `medicine` (`ID`, `genericName`, `brandName`, `type`, isSupply, `isDeleted`, `thresholdQty`) VALUES (NULL, '$supplyName', '', '', '$medicineType', '0', '$thresholdQty')";
+		$stmt = $con->prepare("INSERT INTO medicine (ID, genericName, brandName, type, isSupply, isDeleted, thresholdQty) VALUES (?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("isssiii", $isNull, $supplyName, $brandName, $supplyType, $medicineType, $isSupply, $thresholdQty);
+
+		$supplyName = $_GET['supplyName'];
+		$thresholdQty = $_GET['thresholdQty'];
+		$brandName = '';
+		$supplyType = '';
+		$isSupply = 0;
+		$isNull = NULL;
 
 		$query0 = "SELECT * FROM medicine WHERE genericName='$supplyName'";
 	}
 
 
-	if ($result=mysqli_query($con, $query0)) {
-		if(mysqli_num_rows($result) > 0) {
+
+	if ($result = mysqli_query($con, $query0)) {
+		if (mysqli_num_rows($result) > 0) {
 			$row = mysqli_fetch_array($result);
-			$isDeleted = $row['isDeleted'];
-			if($isDeleted) {
+			$isGenericName = $row['genericName'];
+			$quantityLevel = 0;
+			if ($isGenericName == $brandName || $isGenericName == $supplyName) {
+				$quantityLevel = $row['thresholdQty'] + $thresholdQty;
 				$id = $row['ID'];
-				$query = "UPDATE `medicine` SET `isDeleted` = '0' WHERE `medicine`.`ID` = '$id'";
-				if(mysqli_query($con, $query)) {
-					echo "success";
-				}else {
-					echo "Error";
-				}
-			}else {
-				echo "Already existing record!";
+				$stmt->prepare("UPDATE medicine SET thresholdQty=? WHERE ID=?");
+				$stmt->bind_param("ii", $quantityLevel, $id);
+				$stmt->execute();
+				$unaltered_string = "There is already an existing record! \r\n Quantity has been added to it.";
+				echo nl2br($unaltered_string);
 			}
 		} else {
-			if(mysqli_query($con, $query)) {
-				echo "success";
-			}else {
-				echo "Error";
-			}
+			$stmt->execute();
+			echo "Success!!";
 		}
-	}else {
-		echo "Query Failed";
+	} else {
+		echo "Query failed!";
 	}
 
-
+$stmt->close();
 ?>
