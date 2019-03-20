1ff3f74145cc3bbfad1e81d54eb3a3c71d84f7ad
williamboco <yamyamboco21@gmail.com>
2019-03-20 18:51:16 +0800

    checkbox

--- js/checkbox.js
+++ js/checkbox.js
@@ -3,15 +3,16 @@ window.onload = function() {
 	p.addClass("sorting_disabled");
 
 	$('.checkAll').on('click', function () {
-		
+
 		$(this).closest('table').find('tbody :checkbox')
 		.prop('checked', this.checked)
 		.closest('tr').toggleClass('selected', this.checked);
 	});
-	
+
 }
+
 function selectOne(param) {
 	$(param).closest('tr').toggleClass('selected', param.checked);
 	$(param).closest('table').find('.checkAll').prop('checked', ($(param).closest('table').find('tbody :checkbox:checked').length ==$(param).closest('table').find('tbody :checkbox').length));
-	
+
 }
