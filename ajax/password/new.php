9cc6819de1ec1e593a380025e3e67f4aaabc4aa8
williamboco <yamyamboco21@gmail.com>
2019-03-21 22:35:53 +0800

    new, medicine and user-list

--- ajax/password/new.php
+++ ajax/password/new.php
@@ -5,7 +5,7 @@ include('../../includes/password.php');
 
 $requestID = $_POST['requestID'];
 $password = $_POST['password1'];
-$userID =$_POST['userID'];
+$userID = $_POST['userID'];
 
 //generate hash from input password
 $hashedPassword= generateHash($password);
@@ -13,25 +13,25 @@ $hashedPassword= generateHash($password);
 mysqli_query($con, "UPDATE `user` SET `password` = '$hashedPassword' WHERE `user`.`ID` = '$userID'");
 
 if(mysqli_affected_rows($con) > 0) {
-	
+
 	//update the request as used
 	mysqli_query($con, "UPDATE `password_change_request` SET `isUsed` = '1' WHERE `password_change_request`.`requestID` = '$requestID'");
-	
+
 	if(mysqli_affected_rows($con) > 0) {
 		//get user info
 		$query = mysqli_query($con, "SELECT user.firstName, user.role FROM user WHERE ID='$userID'");
 		$user = mysqli_fetch_array($query);
-		
+
 		$_SESSION['userID']=$userID;
 		$_SESSION['firstName']=$user['firstName'];
 		$_SESSION['role']=$user['role'];
-		
+
 		$message = "success";
 	}else {
 		$message = "Error";
 	}
-	
-	
+
+
 }else {
 	$message = "Password change failed";
 }
--- ajax/table/medicine.php
+++ ajax/table/medicine.php
@@ -2,19 +2,26 @@
 	include('../../includes/dbcon.php');
 	$query = "SELECT * FROM `medicine` WHERE isDeleted=0";
 	$result = mysqli_query($con, $query);
-	
+
 	$medicine = array();
 	while($res = mysqli_fetch_array($result)) {
 		if($res['brandName'] == '') {
 			$mName = $res['genericName'];
-		}else 
+		}else
 			$mName = $res['brandName'];
-		
+
+
 		$x = (object) array(
-			0 => $res['ID'],	
+			0 => $res['ID'],
 			1 => '<input type="checkbox" name="check" onclick="selectOne(this)" id="ID" value="'.$res['ID'].'"/>',
 			2 => '<i class="fa fa-sort-desc details-control" aria-hidden="true"></i>',
-			3 => $mName
+			3 => $mName,
+			4 => $res['availableQty'], //Available Qty
+			5 => '', //Status
+			6 => '<select name="" required><option  value="" disabled selected>Add/Less</option><option value="0">Add</option><option value="1">Less</option></select>',
+			7 => '<input type="number" name="stockQty" min="1" max="100">',
+			8 => '<a type="button" class="btn btn-info">Adjust</a>',
+			9 => '' //Total Qty (upon adjustment)
 		);
 		array_push($medicine, $x);
 	}
