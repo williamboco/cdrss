c5d7e3e6e2c3f44b6e9bf03332f0bccea7859dca
williamboco <yamyamboco21@gmail.com>
2019-03-23 17:49:44 +0800

    filtered_users_avp and userDelete

--- ajax/filtered_users_avp.php
+++ ajax/filtered_users_avp.php
@@ -1,19 +1,24 @@
 <?php
 include('../includes/dbcon.php');
 
-$query = "SELECT * FROM `user` WHERE isActive=1 || 0";
+$query = "SELECT * FROM `user`";
 $result = mysqli_query($con, $query);
-
 
 $users = array();
 while($row = mysqli_fetch_array($result)) {
+	if($row['isActive'] == 1) {
+		$status = "Active";
+	} else {
+		$status = "Inactive";
+ 	}
+
 	$x = (object) array(
 		0 => '',
 		1 => $row['ID'],
 		2 => $row['firstName'].' '.$row['lastName'],
 		3 => $row['email'],
 		4 => $row['role'],
-		5 => $row['isActive'],
+		5 => $status,
 		6 => '<button id="ID" class="btn btn-primary pull-right" onclick="viewUser(this.value)" value="'.$row['ID'].'"> <i class="glyphicon glyphicon-edit"></i> Update Status</button>'
 	);
 	array_push($users, $x);
--- ajax/userDelete.php
+++ ajax/userDelete.php
@@ -2,25 +2,31 @@
 include('../includes/dbcon.php');
 $id = $_GET['userID'];
 
-$query = "UPDATE user SET `isActive` = '0' WHERE user.`ID` = '$id'";
+$statusType = $_GET['statusType'];
+
 $result = mysqli_query($con, "SELECT * FROM `user` WHERE ID='$id'");
 $res = mysqli_fetch_array($result);
+
+if($statusType == 1)
+		$query = "UPDATE user SET `isActive` = '1' WHERE user.`ID` = '$id'";
+else
+		$query = "UPDATE user SET `isActive` = '0' WHERE user.`ID` = '$id'";
 
 if($res['role'] == 'Admin') {
 	$result = mysqli_query($con, "SELECT * FROM `user` WHERE role='Admin' AND isActive='1'");
 
 	if(mysqli_num_rows($result) > 1) {
 		if(mysqli_query($con, $query)) {
-			echo "success";
+			echo 'Success.';
 		}else
 			echo "Query failed";
 	}else
 		echo "Cannot delete account";
-	
+
 }else {
-	
+
 	if(mysqli_query($con, $query)) {
-		echo "success";
+		echo "<h4><b>Status updated successfully.</b></h4>";
 	}else
 		echo "Query failed";
 }
