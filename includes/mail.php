51761736fe266c8e094f9a4352494f700f4acd47
williamboco <yamyamboco21@gmail.com>
2019-03-27 20:51:02 +0800

    mail and password

--- includes/mail.php
+++ includes/mail.php
@@ -1,49 +1,45 @@
 <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 // Load Composer's autoloader
 require 'vendor/autoload.php';
 
-$mail = new PHPMailer(true);
+$mail = new PHPmailer(true);
 
-$mail->SMTPDebug = 3;                               // Enable verbose debug output
+try {
+    //Server settings
+    $mail->SMTPDebug = 3;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'cdrsiac@gmail.com';                     // SMTP username
+    $mail->Password   = 'capstone_project';
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );                                        // SMTP password
+    $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'cdrsiac@gmail.com';                 // SMTP username
-$mail->Password = 'capstone_project';
-$mail->SMTPOptions = array(
-    'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-    )
-);                          // SMTP password
-//$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+    //Recipients
+    $mail->setFrom('cdrsiac@gmail.com', 'iACADEMY CDRS');
 
-$mail->setFrom('cdrsiac@gmail.com');
-//$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-$mail->addAddress($email);               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
+    $mail->addAddress($email);     // Add a recipient
 
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'CDRS';
+    $mail->Body    = $msg;
+    $mail->AltBody = 'This is a text';
 
-$mail->Subject = 'CDRS';
-$mail->Body    = $msg;
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-if(!$mail->send()) {
-    $message = 'Message could not be sent.';
-    $message .= 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    $message = 'Message has been sent';
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-
-?>
--- includes/password.php
+++ includes/password.php
@@ -4,33 +4,23 @@
 		$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
 		$pass = array(); //remember to declare $pass as an array
 		$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-		for ($i = 0; $i < 9; $i++) {
+		for ($i = 0; $i < 8; $i++) {
 			$n = rand(0, $alphaLength);
 			$pass[] = $alphabet[$n];
 		}
 		return implode($pass); //turn the array into a string
 	}
 
-	function passwordEncrypt($pass) {
-			$hashFormat = "2y$11$";
-			$salt = generateSalt();
-			$format_and_salt = $hashFormat . $salt;
-			$hash = crypt($pass, $format_and_salt);
-			return ($hash);
-	}
-
 	//function to generate hash
-	function generateSalt() {
-			$unique_random_string = md5(uniqid(mt_rand(), true));
-			$base64_string = base64_encode($unique_random_string);
-			$modified_base64_string = str_replace('+', '.', $base64_string);
-			$salt = substr($modified_base64_string, 0, 22);
-			return ($salt);
+	function generateHash($pass) {
+		if(defined("CRYPT_BLOWFISH") && CRYPT_BLOWFISH) {
+			$salt = '$2y$11$'.substr(md5(uniqid(rand(), true)), 0, 22);
+			return crypt($pass, $salt);
 		}
-
+	}
 
 	//function to verify password
 	function verify($password, $hashedPassword) {
-		return(crypt($password, $hashedPassword) == $hashedPassword);	
+		return htmlspecialchars_decode($password, $hashedPassword) == $hashedPassword;
 	}
 ?>
