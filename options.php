cfdfd6536fe02819394575946068459333a0f782
williamboco <yamyamboco21@gmail.com>
2019-03-31 10:27:53 +0800

    options

--- options.php
+++ options.php
@@ -19,6 +19,7 @@ if($_SESSION['role'] == 'Admin') {
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Options</title>
 </head>
 <body>
 	<?php
