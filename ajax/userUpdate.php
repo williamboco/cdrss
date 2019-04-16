a91a97f13bcbc5b6bc25a5d27b3003d074e7c0f2
williamboco <yamyamboco21@gmail.com>
2019-04-16 17:58:25 +0800

    userUpdate

--- ajax/userUpdate.php
+++ ajax/userUpdate.php
@@ -2,14 +2,22 @@
 session_start();
 include('../includes/dbcon.php');
 
-$id = $_POST['idNumber'];
-$firstName = $_POST['firstname'];
-$lastName = $_POST['lastname'];
-$email = $_POST['email'];
-$employed = $_POST['dateEmployed'];
-$gender = $_POST['gender'];
-$contact = $_POST['contact'];
-$userID = $_POST['userID'];
+$method = 'aes-256-cbc';
+$password1 = '3sc3RLrpd17';
+$key = substr(hash('sha256', $password1, true), 0, 32);
+$iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
+
+$id = htmlspecialchars($_POST['idNumber']);
+$firstName = htmlspecialchars($_POST['firstname']);
+$firstName = base64_encode(openssl_encrypt($firstName, $method, $key, OPENSSL_RAW_DATA, $iv));
+$lastName = htmlspecialchars($_POST['lastname']);
+$lastName = base64_encode(openssl_encrypt($lastName, $method, $key, OPENSSL_RAW_DATA, $iv));
+$email = htmlspecialchars($_POST['email']);
+$email = base64_encode(openssl_encrypt($email, $method, $key, OPENSSL_RAW_DATA, $iv));
+$employed = htmlspecialchars($_POST['dateEmployed']);
+$gender = htmlspecialchars($_POST['gender']);
+$contact = htmlspecialchars($_POST['contact']);
+$userID = htmlspecialchars($_POST['userID']);
 
 
 if ($result=mysqli_query($con,"SELECT ID FROM user WHERE ID=$id")) {
@@ -18,14 +26,14 @@ if ($result=mysqli_query($con,"SELECT ID FROM user WHERE ID=$id")) {
 	} else {
 		$query = "UPDATE `user` SET `ID` = '$id', `email` = '$email', `dateEmployed` = '$employed', `firstName` = '$firstName', `lastName` = '$lastName', `gender` = '$gender', `contact` = '$contact' WHERE `user`.`ID` = '$userID'";
 		mysqli_query($con,$query);
-		
+
 		if(mysqli_affected_rows($con) > 0) {
 			$_SESSION['userID'] = $id;
 			$message = "success";
 		}else {
 			$message = "Not updated.";
 		}
-		
+
 	}
 }else {
 	$message = "Query Failed.";
