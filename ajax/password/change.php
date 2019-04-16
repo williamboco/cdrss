7a95d27a17d217a3eff50118ccf060ebc7b2cfc5
williamboco <yamyamboco21@gmail.com>
2019-04-16 18:19:40 +0800

    password change

--- ajax/password/change.php
+++ ajax/password/change.php
@@ -1,26 +1,49 @@
 <?php
 include('../../includes/dbcon.php');
-include('../../includes/password.php');
 
-$password = $_POST['oldPass'];
-$newPass = $_POST['password1'];
-$userID =$_POST['userID'];
+$method = 'aes-256-cbc';
+$password1 = '3sc3RLrpd17';
+$key = substr(hash('sha256', $password1, true), 0, 32);
+$iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
+
+$password = htmlspecialchars($_POST['oldPass']);
+$newPass = htmlspecialchars($_POST['password1']);
+
 
 //db query
-$query = mysqli_query($con, "SELECT * FROM user WHERE ID='$userID'");
-$row= mysqli_fetch_array($query);
+$query = $con->prepare("SELECT * FROM user WHERE ID=?");
+$query->bind_param("i", $userID);
 
-if(mysqli_num_rows($query) > 0 && verify($password, $row['password'])) {
+$userID = $_POST['userID'];
 
-	//generate hash from input password
-	$hashedPassword = generateHash($newPass);
-	mysqli_query($con, "UPDATE `user` SET `password` = '$hashedPassword', `datePassChanged` = NOW() WHERE `user`.`ID` = '$userID'");
+$query->execute();
+$result = $query->get_result();
+$rownum = mysqli_num_rows($result);
 
-	if(mysqli_affected_rows($con) > 0) {
-		$message = "Success";
-	}else {
-		$message = "Not updated";
+
+if($rownum > 0) {
+
+	while ($row = $result->fetch_assoc()) {
+		$row['password'] = openssl_decrypt(base64_decode($row['password']), $method, $key, OPENSSL_RAW_DATA, $iv);
+
+		if ($row['password'] == $password) {
+			//generate hash from input password
+			$hashedPassword = base64_encode(openssl_encrypt($newPass, $method, $key, OPENSSL_RAW_DATA, $iv));
+			mysqli_query($con, "UPDATE `user` SET `password` = '$hashedPassword', `datePassChanged` = NOW() WHERE `user`.`ID` = '$userID'");
+
+			if(mysqli_affected_rows($con) > 0) {
+				$message = "Success";
+			}else {
+				$message = "Not updated";
+			}
+
+		}
+
 	}
+
+	 $message = "Not Updated!";
+
+
 
 }else {
 
