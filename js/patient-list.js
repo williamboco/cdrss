b2d1b776ff54065ba1c8a3bd9b6166584720a029
williamboco <yamyamboco21@gmail.com>
2019-04-22 09:01:43 +0800

    patient and users list

--- js/patient-list.js
+++ js/patient-list.js
@@ -15,15 +15,15 @@ $(document).ready(function() {
 	],
 	"order": [[ 2, 'asc' ]]
 	} );
-	
+
 	t.on( 'order.dt search.dt', function () {
 		t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
 			cell.innerHTML = i+1;
 		} );
 	} ).draw();
-	
+
 	$('.filters').trigger('change');
-	
+
 } );
 
 
@@ -40,7 +40,7 @@ $('.filters').on('change', function() {
 		success: function(data) {
 			var obj = JSON.parse(data);
 			obj = obj.data;
-			
+
 			datatable.clear();
 			datatable.rows.add(obj);
 			datatable.draw();
@@ -49,18 +49,18 @@ $('.filters').on('change', function() {
 });
 
 $( "#patientAddForm" ).on( "submit", function( event ) {
-	var $form = $( this );
+	var $form = $(this);
 
 	event.preventDefault();
 	//console.log($form.serialize());
-  
+
 	// Use Ajax to submit form data
 	$.ajax({
 		type: 'POST',
 		url: $form.attr('action'),
 		data: $form.serialize(),
 		success: function(result) {
-		
+
 		$("#patientModal").modal('hide');
 		if(result=='success') {
 			alertify.log("Patient record inserted");
@@ -68,7 +68,7 @@ $( "#patientAddForm" ).on( "submit", function( event ) {
 		}else {
 			alertify.log(result);
 		}
-		
+
 		}
 	});
 });
