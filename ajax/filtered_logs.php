1b56bf6da772351d8faf4925de395b8e2966703b
williamboco <yamyamboco21@gmail.com>
2019-04-29 09:58:18 +0800

    decrypted logs

--- ajax/filtered_logs.php
+++ ajax/filtered_logs.php
@@ -3,6 +3,11 @@ include('../includes/dbcon.php');
 
 $query = "SELECT * FROM `logs` JOIN `user` ON user.ID = logs.userID";
 
+$method = 'aes-256-cbc';
+$password = '3sc3RLrpd17';
+$key = substr(hash('sha256', $password, true), 0, 32);
+$iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
+
 $result = mysqli_query($con, $query);
 $users = array();
 while($row = mysqli_fetch_array($result)) {
@@ -11,6 +16,10 @@ while($row = mysqli_fetch_array($result)) {
 	} else {
 		$status = "Inactive";
  	}*/
+
+	$row['firstName'] = openssl_decrypt(base64_decode($row['firstName']), $method, $key, OPENSSL_RAW_DATA, $iv);
+	$row['lastName'] = openssl_decrypt(base64_decode($row['lastName']), $method, $key, OPENSSL_RAW_DATA, $iv);
+	$row['role'] = openssl_decrypt(base64_decode($row['role']), $method, $key, OPENSSL_RAW_DATA, $iv);
 
 	$x = (object) array(
 		0 => $row['eventID'],
