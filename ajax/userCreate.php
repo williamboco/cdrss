ba37545e5d5ff5377e2c3238d8b7fc43224b3b2d
williamboco <yamyamboco21@gmail.com>
2019-05-08 00:16:37 +0800

    Reset Password and User Create emails are fully functional

--- ajax/userCreate.php
+++ ajax/userCreate.php
@@ -91,7 +91,7 @@ if ($rownum > 0) {
 				 $stmt->execute();
 
 				 $query2 = $con->prepare("INSERT INTO `password_change_request` (ID, requestID, userID, requestDate, isUsed) VALUES (?,?,?,NOW(),?)");
-				 $query2->bind_param("isii", $isNull, $requestID, $id, $isUsed);
+				 $query2->bind_param("issi", $isNull, $requestID, $id, $isUsed);
 				 $isNull = NULL;
 				 $requestID = randomPassword();
 				 $id = htmlspecialchars($_POST['idNumber']);
@@ -102,9 +102,9 @@ if ($rownum > 0) {
 				 $email = openssl_decrypt(base64_decode($email), $method, $key, OPENSSL_RAW_DATA, $iv);
 
 				 // email message
-			   // $title = "link";
-				 //$link = $_SERVER['SERVER_NAME']."/cdrs/pass-new.php?rID=".$requestID;
-				 $msg = "The new iACADEMY account password is iacademyCDRS. You may now login to your account using the new password";// "New iAcademy CDRS Account password. \nPlease click this <a href='".$link."'>".$title."</a> to create new password.";
+			   $title = "link";
+				 $link = "http://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']."/cdrs/pass-new.php?rID=".$requestID;
+				 $msg = "New iAcademy CDRS Account password. \nPlease click this <a href='".$link."'>".$title."</a> to create new password.";
 
 				 // To send HTML mail, the Content-type header must be set
 				 $headers  = 'MIME-Version: 1.0' . "\r\n";
