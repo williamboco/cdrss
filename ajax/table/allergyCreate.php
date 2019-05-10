8111732222fc0c0e4eab92da5f50771462d5ba91
williamboco <yamyamboco21@gmail.com>
2019-05-10 09:51:10 +0800

    allergyCreate

--- ajax/table/allergyCreate.php
+++ ajax/table/allergyCreate.php
@@ -6,54 +6,59 @@
 	$allergyName = htmlspecialchars($_GET['allergyName']);
 	$description = htmlspecialchars($_GET['description']);
 
-	$stmt = $con->prepare("INSERT INTO `allergy` (ID, allergyName, description, isDeleted) VALUES (?, ?, ?, ?)");
-	$stmt->bind_param("issi", $isNull, $allergyName, $description, $isDeleted);
+ if (ctype_space($allergyName) || ctype_space($description)) {
+	 echo "Whitespaces are not allowed. Please enter valid input";
+ } else {
+	 $stmt = $con->prepare("INSERT INTO `allergy` (ID, allergyName, description, isDeleted) VALUES (?, ?, ?, ?)");
+ 	$stmt->bind_param("issi", $isNull, $allergyName, $description, $isDeleted);
 
-	$isNull = NULL;
-	$isDeleted = 0;
+ 	$isNull = NULL;
+ 	$isDeleted = 0;
 
-	$query0 = "SELECT * FROM `allergy` WHERE allergyName='$allergyName'";
+ 	$query0 = "SELECT * FROM `allergy` WHERE allergyName='$allergyName'";
 
-	if ($result=mysqli_query($con, $query0)) {
-		if(mysqli_num_rows($result) > 0) {
-			$row = mysqli_fetch_array($result);
-			$isDeleted = $row['isDeleted'];
+ 	if ($result=mysqli_query($con, $query0)) {
+ 		if(mysqli_num_rows($result) > 0) {
+ 			$row = mysqli_fetch_array($result);
+ 			$isDeleted = $row['isDeleted'];
 
-			if($isDeleted) {
-				$stmt = $con->prepare("UPDATE `allergy` SET isDeleted=? WHERE ID=?");
-				$stmt->bind_param("ii", $isDeleted, $id);
+ 			if($isDeleted) {
+ 				$stmt = $con->prepare("UPDATE `allergy` SET isDeleted=? WHERE ID=?");
+ 				$stmt->bind_param("ii", $isDeleted, $id);
 
-				$isDeleted = 0;
-				$id = $row['ID'];
-				$stmt->execute();
+ 				$isDeleted = 0;
+ 				$id = $row['ID'];
+ 				$stmt->execute();
 
-					if(mysqli_query($con, $query0)) {
-						echo "Record successfully added";
-					}else {
-						echo "Error: Record was not added";
-					}
-			} else {
-				echo "There is already an existing record!";
-			}
-		} else {
-			$stmt->execute();
+ 					if(mysqli_query($con, $query0)) {
+ 						echo "Record successfully added";
+ 					}else {
+ 						echo "Error: Record was not added";
+ 					}
+ 			} else {
+ 				echo "There is already an existing record!";
+ 			}
+ 		} else {
+ 			$stmt->execute();
 
-			if(mysqli_query($con, $query0)) {
-				echo "Record successfully added";
-			}else {
-				echo "Error: Record was not added";
-			}
-		}
-	}else {
-		echo "Error: Query failed";
-	}
+ 			if(mysqli_query($con, $query0)) {
+ 				echo "Record successfully added";
+ 			}else {
+ 				echo "Error: Record was not added";
+ 			}
+ 		}
+ 	}else {
+ 		echo "Error: Query failed";
+ 	}
 
-	 $stmt = $con->prepare("INSERT INTO logs (eventID, eventDate, eventName,   userID) VALUES (?, NOW(), ?, ?)");
-	 $stmt->bind_param("isi", $eventID, $eventName, $userID);
-	 $eventID = NULL;
-	 $userID = $_SESSION['userID'];
-	 $eventName = "Created allergy.";
-	 $stmt->execute();
+ 	 $stmt = $con->prepare("INSERT INTO logs (eventID, eventDate, eventName,   userID) VALUES (?, NOW(), ?, ?)");
+ 	 $stmt->bind_param("isi", $eventID, $eventName, $userID);
+ 	 $eventID = NULL;
+ 	 $userID = $_SESSION['userID'];
+ 	 $eventName = "Created allergy.";
+ 	 $stmt->execute();
 
-	 $stmt->close();
+ 	 $stmt->close();
+ }
+
 ?>
