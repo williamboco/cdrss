c0b3bc4ea50b8c90440e7123d7a70707c93a9361
williamboco <yamyamboco21@gmail.com>
2019-05-10 12:05:53 +0800

    medicineUpdate

--- ajax/table/medicineUpdate.php
+++ ajax/table/medicineUpdate.php
@@ -5,52 +5,66 @@
 	$id = $_GET['id'];
 	$isSupply = $_GET['isSupply'];
 
-	if($isSupply == '0') {
-
-		$stmt = $con->prepare("UPDATE `medicine` SET brandName=?, genericName=?, type=?, dosage=?, dosageQty=?, isSupply=? WHERE ID=?");
-		$stmt->bind_param("ssssiii", $brandName, $genericName, $medicineType, $dosage, $dosageQty, $isSupply, $id);
-
+	if ($isSupply == '0') {
 		$brandName = htmlspecialchars($_GET['brandName']);
 		$genericName = htmlspecialchars($_GET['genericName']);
-		$medicineType = $_GET['medicineType'];
-		$dosage = $_GET['medicineDosage'];
-		$dosageQty = htmlspecialchars($_GET['medicineDosageQty']);
-		$isSupply = $_GET['isSupply'];
-
-		$query0 = "SELECT * FROM `medicine` WHERE brandName='$brandName' AND genericName='$genericName'";
-
-	}else {
-
-		$stmt = $con->prepare("UPDATE `medicine` SET genericName=?, dosage=?, dosageQty=?, isSupply=? WHERE ID=?");
-		$stmt->bind_param("ssiii", $supplyName, $dosage, $dosageQty, $isSupply, $id);
-
-		$supplyName = htmlspecialchars($_GET['supplyName']);
-		$dosage = $_GET['supplyDosage'];
-		$dosageQty = htmlspecialchars($_GET['supplyDosageQty']);
-		$isSupply = $_GET['isSupply'];
-
-		$query0 = "SELECT * FROM `medicine` WHERE genericName='$supplyName'";
+	} else {
+		$genericName = htmlspecialchars($_GET['supplyName']);
+		$brandName = '';
 	}
 
-	if ($result = mysqli_query($con, $query0)) {
-		$row = mysqli_fetch_array($result);
-		$rowId = $row['ID'];
-		if(mysqli_num_rows($result) > 0 && $rowId != $id) {
-			echo "Error: Unable to update record";
-		} else {
-				$stmt->execute();
-				echo "Record successfully updated";
+	if (ctype_space($brandName) || ctype_space($genericName)) {
+		echo "Whitespaces are not allowed. Please enter valid input";
+	} else {
+		if($isSupply == '0') {
+
+			$stmt = $con->prepare("UPDATE `medicine` SET brandName=?, genericName=?, type=?, dosage=?, dosageQty=?, isSupply=? WHERE ID=?");
+			$stmt->bind_param("ssssiii", $brandName, $genericName, $medicineType, $dosage, $dosageQty, $isSupply, $id);
+
+			$brandName = htmlspecialchars($_GET['brandName']);
+			$genericName = htmlspecialchars($_GET['genericName']);
+			$medicineType = $_GET['medicineType'];
+			$dosage = $_GET['medicineDosage'];
+			$dosageQty = htmlspecialchars($_GET['medicineDosageQty']);
+			$isSupply = $_GET['isSupply'];
+
+			$query0 = "SELECT * FROM `medicine` WHERE brandName='$brandName' AND genericName='$genericName'";
+
+		}else {
+
+			$stmt = $con->prepare("UPDATE `medicine` SET genericName=?, dosage=?, dosageQty=?, isSupply=? WHERE ID=?");
+			$stmt->bind_param("ssiii", $supplyName, $dosage, $dosageQty, $isSupply, $id);
+
+			$supplyName = htmlspecialchars($_GET['supplyName']);
+			$dosage = $_GET['supplyDosage'];
+			$dosageQty = htmlspecialchars($_GET['supplyDosageQty']);
+			$isSupply = $_GET['isSupply'];
+
+			$query0 = "SELECT * FROM `medicine` WHERE genericName='$supplyName'";
 		}
-	}else {
-		echo "Error:" . mysqli_error($con);
+
+		if ($result = mysqli_query($con, $query0)) {
+			$row = mysqli_fetch_array($result);
+			$rowId = $row['ID'];
+			if(mysqli_num_rows($result) > 0 && $rowId != $id) {
+				echo "Error: Unable to update record";
+			} else {
+					$stmt->execute();
+					echo "Record successfully updated";
+			}
+		}else {
+			echo "Error:" . mysqli_error($con);
+		}
+
+		$stmt = $con->prepare("INSERT INTO logs (eventID, eventDate, eventName,   userID) VALUES (?, NOW(), ?, ?)");
+		$stmt->bind_param("isi", $eventID, $eventName, $userID);
+		$eventID = NULL;
+		$userID = $_SESSION['userID'];
+		$eventName = "Updated medicine.";
+		$stmt->execute();
+
+		$stmt->close();
 	}
 
-	$stmt = $con->prepare("INSERT INTO logs (eventID, eventDate, eventName,   userID) VALUES (?, NOW(), ?, ?)");
-	$stmt->bind_param("isi", $eventID, $eventName, $userID);
-	$eventID = NULL;
-	$userID = $_SESSION['userID'];
-	$eventName = "Updated medicine.";
-	$stmt->execute();
 
-	$stmt->close();
 ?>
