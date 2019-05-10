54dee835a2a41802121784264a965e6665279f7e
williamboco <yamyamboco21@gmail.com>
2019-05-10 09:53:08 +0800

    allergyUpdate

--- ajax/table/allergyUpdate.php
+++ ajax/table/allergyUpdate.php
@@ -6,36 +6,42 @@
 	$allergyName = htmlspecialchars($_GET['allergyName']);
 	$description = htmlspecialchars($_GET['description']);
 
-	$stmt = $con->prepare("UPDATE `allergy` SET allergyName=?, description=? WHERE ID=?");
-	$stmt->bind_param("ssi", $allergyName, $description, $id);
+	if(ctype_space($allergyName) || ctype_space($description)) {
+		echo "Whitespaces are not allowed. Please enter valid input";
+	} else {
+		$stmt = $con->prepare("UPDATE `allergy` SET allergyName=?, description=? WHERE ID=?");
+		$stmt->bind_param("ssi", $allergyName, $description, $id);
 
-	$query0 = "SELECT * FROM `allergy` WHERE allergyName='$allergyName'";
+		$query0 = "SELECT * FROM `allergy` WHERE allergyName='$allergyName'";
 
-	if ($result=mysqli_query($con, $query0)) {
-		$row = mysqli_fetch_array($result);
-		$rowId = $row['ID'];
+		if ($result=mysqli_query($con, $query0)) {
+			$row = mysqli_fetch_array($result);
+			$rowId = $row['ID'];
 
-		if(mysqli_num_rows($result) > 0 && $rowId != $id) {
-			echo "Error: Duplicate Record";
-		} else {
+			if(mysqli_num_rows($result) > 0 && $rowId != $id) {
+				echo "Error: Duplicate Record";
+			} else {
 
-			$stmt->execute();
-			if(mysqli_query($con, $query0)) {
-				echo "Record successfully updated";
-			}else {
-				echo "Error: Record was not updated";
+				$stmt->execute();
+				if(mysqli_query($con, $query0)) {
+					echo "Record successfully updated";
+				}else {
+					echo "Error: Record was not updated";
+				}
 			}
+		}else {
+			echo "Error: Query Failed";
 		}
-	}else {
-		echo "Error: Query Failed";
+
+		 $stmt = $con->prepare("INSERT INTO logs (eventID, eventDate, eventName,   userID) VALUES (?, NOW(), ?, ?)");
+		 $stmt->bind_param("isi", $eventID, $eventName, $userID);
+		 $eventID = NULL;
+		 $userID = $_SESSION['userID'];
+		 $eventName = "Updated allergy";
+		 $stmt->execute();
+
+		 $stmt->close();
 	}
 
-	 $stmt = $con->prepare("INSERT INTO logs (eventID, eventDate, eventName,   userID) VALUES (?, NOW(), ?, ?)");
-	 $stmt->bind_param("isi", $eventID, $eventName, $userID);
-	 $eventID = NULL;
-	 $userID = $_SESSION['userID'];
-	 $eventName = "Updated allergy";
-	 $stmt->execute();
 
-	 $stmt->close();
 ?>
