511cfb63095253d7ea758e58780ef51b27b5a741
williamboco <yamyamboco21@gmail.com>
2019-05-12 08:23:35 +0800

    userUpdate

--- ajax/userUpdate.php
+++ ajax/userUpdate.php
@@ -11,45 +11,60 @@ $id = htmlspecialchars($_POST['idNumber']);
 $firstName = htmlspecialchars($_POST['firstname']);
 $lastName = htmlspecialchars($_POST['lastname']);
 $email = htmlspecialchars($_POST['email']);
-$email = base64_encode(openssl_encrypt($email, $method, $key, OPENSSL_RAW_DATA, $iv));
+$email = isset($email) ? trim($email) : null;
+//$email = base64_encode(openssl_encrypt($email, $method, $key, OPENSSL_RAW_DATA, $iv));
 $employed = htmlspecialchars($_POST['dateEmployed']);
 $gender = htmlspecialchars($_POST['gender']);
 $contact = htmlspecialchars($_POST['contact']);
 $userID = htmlspecialchars($_POST['userID']);
 
+$allowed = [
+	'iacademy.edu.ph'
+];
 
-if (!ctype_alpha($firstName) || !ctype_alpha($lastName)) {
-	echo "Error: Input must only contain letters.";
-} else if ($result=mysqli_query($con,"SELECT ID FROM user WHERE ID=$id")) {
-	if(mysqli_num_rows($result) > 0 && $userID != $id) {
-		echo "User with that ID number already exists.";
-	} else {
-			if (ctype_space($firstName) || ctype_space($lastName)) {
-				$message = "Whitespaces are not not allowed. Please enter a valid input";
+$message = "";
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	$parts = explode('@', $email);
+	$domain = array_pop($parts);
+
+	if (! in_array($domain, $allowed)) {
+		echo "The email must end in iacademy.edu.ph";
+	} else 	if (!ctype_alpha($firstName) || !ctype_alpha($lastName)) {
+			echo "Error: Input must only contain letters.";
+		} else if ($result=mysqli_query($con,"SELECT ID FROM user WHERE ID=$id")) {
+			if(mysqli_num_rows($result) > 0 && $userID != $id) {
+				echo "User with that ID number already exists.";
 			} else {
-				$firstName = base64_encode(openssl_encrypt($firstName, $method, $key, OPENSSL_RAW_DATA, $iv));
-				$lastName = base64_encode(openssl_encrypt($lastName, $method, $key, OPENSSL_RAW_DATA, $iv));
-				$stmt = $con->prepare("UPDATE `user` SET `ID` = ?, `email` = ?, `dateEmployed` = ?, `firstName` = ?, `lastName` = ?, `gender` = ?, `contact` = ? WHERE `user`.`ID` = ?");
-				$stmt->bind_param("ssssssis", $id, $email, $employed, $firstName, $lastName, $gender, $contact, $userID);
-				$stmt->execute();
+					if (ctype_space($firstName) || ctype_space($lastName)) {
+						$message = "Whitespaces are not not allowed. Please enter a valid input";
+					} else {
+						$firstName = base64_encode(openssl_encrypt($firstName, $method, $key, OPENSSL_RAW_DATA, $iv));
+						$lastName = base64_encode(openssl_encrypt($lastName, $method, $key, OPENSSL_RAW_DATA, $iv));
+						$stmt = $con->prepare("UPDATE `user` SET `ID` = ?, `email` = ?, `dateEmployed` = ?, `firstName` = ?, `lastName` = ?, `gender` = ?, `contact` = ? WHERE `user`.`ID` = ?");
+						$stmt->bind_param("ssssssis", $id, $email, $employed, $firstName, $lastName, $gender, $contact, $userID);
+						$stmt->execute();
 
-				if(mysqli_affected_rows($con) > 0) {
-				 $message = "Profile is successfully updated.";
-				}else {
-				 $message = "Profile is not updated.";
-				}
+						if(mysqli_affected_rows($con) > 0) {
+						 $message = "Profile is successfully updated.";
+						}else {
+						 $message = "Profile is not updated.";
+						}
 
-			 	$stmt = $con->prepare("INSERT INTO logs (eventID, eventDate, eventName, userID) VALUES (?, NOW(), ?, ?)");
-			 	$stmt->bind_param("isi", $eventID, $eventName, $userID);
-			 	$eventID = NULL;
-			 	$userID = $_SESSION['userID'];
-			 	$eventName = "Updated user profile.";
-			 	$stmt->execute();
+					 	$stmt = $con->prepare("INSERT INTO logs (eventID, eventDate, eventName, userID) VALUES (?, NOW(), ?, ?)");
+					 	$stmt->bind_param("isi", $eventID, $eventName, $userID);
+					 	$eventID = NULL;
+					 	$userID = $_SESSION['userID'];
+					 	$eventName = "Updated user profile.";
+					 	$stmt->execute();
+					}
 			}
-	}
-}else {
+		}else {
 
-		$message = "Query Failed.";
+				$message = "Query Failed.";
+		}
+
+
 }
+
 echo $message;
 ?>
