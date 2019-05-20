b6b355550658e37bfc8083a4155f6c9d5e6ed2f1
lorenfleur <201601048@iacademy.edu.ph>
2019-05-20 16:08:23 +0800

    fixed profileForm.js - go back on delete

--- js/profileForm.js
+++ js/profileForm.js
@@ -392,7 +392,7 @@ $( "#patientEditForm" ).on( "submit", function( event ) {
 
 			if(response[0].includes("success")) {
 				alertify.alert(response[0]).set('onok', function(closeEvent){
-					window.location.reload(true);
+					window.history.back();
 				});
 			}else {
 				alertify.alert(response);
