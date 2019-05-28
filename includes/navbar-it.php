b687afee5bfc338edb9c6ec052f35b305f61dbad
williamboco <yamyamboco21@gmail.com>
2019-05-28 22:24:09 +0800

    navbar-it

--- includes/navbar-it.php
+++ includes/navbar-it.php
@@ -13,6 +13,7 @@
       </div>
     <div class="collapse navbar-collapse" id="navigationbar">
       <ul class="nav navbar-nav">
+        <li><a href="home-it.php">Home</a> </li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li>
